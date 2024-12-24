1a7337709b0311fa5c59b7e6b75b7a022888d944
Janak Chaudhary <janakchy01@gmail.com>
2024-12-25 00:09:45 +0545

    BookServlet created

---
+++ src/main/java/servlets/BookServlet.java
@@ -0,0 +1,64 @@
+package servlets;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.List;
+import com.google.gson.Gson;
+import model.Book;
+import services.BookService;
+
+@WebServlet("/books")
+public class BookServlet extends HttpServlet {
+
+    private Gson gson = new Gson();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        List<Book> books = BookService.getAllBooks();
+        String json = gson.toJson(books);
+        System.out.println("Get");
+        response.getWriter().write(json);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String isbn = request.getParameter("isbn");
+
+        if (name != null && isbn != null) {
+            Book book = new Book(name, isbn);
+            BookService.addBook(book);
+            response.setStatus(HttpServletResponse.SC_CREATED);
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String isbn = request.getParameter("isbn");
+    System.out.println("isbn"+isbn);
+        if (isbn != null) {
+            BookService.deleteBook(isbn);
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String isbn = request.getParameter("isbn");
+        String name = request.getParameter("name");
+
+        if (isbn != null && name != null) {
+            BookService.updateBook(isbn, name);
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+}
